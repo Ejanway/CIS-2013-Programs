42616dbf1644c46f871225fc51c57df3c3145f2a
ejanway <112423991+ejanway@users.noreply.github.com>
2022-09-23 16:31:30 -0500

    Update mpg.js

--- mpg.js
+++ mpg.js
@@ -1,6 +1,7 @@
-     var intMiles, floatGallons, floatMpg;
-     intMiles = parseInt(prompt("Enter miles driven"));
-     floatGallons = prompt("Enter gallons of gas used");
-     floatGallons = parseFloat(floatGallons);
-     floatMpg = parseFloat(intMiles/floatGallons);
-     alert("Miles per gallon = " + floatMpg);
+var= floatAge, floatDays, intWeeks, floatMonths, intFortnights
+floatAge= ("Enter age in years") 
+floatDays = parseFloat(floatAge*365.25);
+floatMonths = prompt(floatAge*12);
+intWeeks = parseFloat(floatDays/7);
+intFortnights = parseFloat(floatDays/14).toFixed(2);
+alert("Age"+ "Days"+ "Weeks"+"Months"+"Fortnights");  
