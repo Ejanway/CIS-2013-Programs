ad93b15a57956e3916f3d8a5ae98dc991508e016
ejanway <112423991+ejanway@users.noreply.github.com>
2022-09-23 20:21:55 -0500

    Add files via upload

---
+++ age.js
@@ -0,0 +1,11 @@
+var floatAge;
+var floatDays;
+var intWeeks;
+var floatMonths;
+var intFortnights;
+floatAge= promt("Enter age in years"); 
+floatDays= floatAge*365.26;
+floatMonths= floatAge*12;
+intWeeks= floatDays/14;
+
+
