1c428c36aa7ee6fde1df5f143f0fece885fc7d46
ejanway <112423991+ejanway@users.noreply.github.com>
2022-09-23 21:50:37 -0500

    Update age.js

--- age.js
+++ age.js
@@ -1,11 +1,14 @@
-var floatAge;
-floatAge= promt("Enter age in years");
-var floatDays;
-floatDays= (floatAge*365.26);
-var intWeeks;
-intWeeks= (floatDays/7);
-var floatMonths;
-floatMonths= (floatAge*12)
-var intFortnights;
-intFortnights= (floatDays/14);
-alert("Age=" + floatMonths + intWeeks + floatDays + intFortnights);
+var floatAge
+floatAge = prompt("Enter age in years");
+var floatDays
+floatDays = (floatAge * (365.25));
+var intWeeks
+intWeeks = (floatDays / (7));
+var floatMonths
+floatMonths = (floatAge * (12));
+var intFortnights
+intFortnights = (floatDays / (14));
+alert("months= " + floatMonths);
+alert("weeks = " + intWeeks);
+alert("Days = " + floatDays);
+alert("Fortnights = " + intFortnights);
