fafadbd115684ac91bd5f0a8a445aa760834c3f6
ejanway <112423991+ejanway@users.noreply.github.com>
2022-10-07 15:16:21 -0500

    Add files via upload

---
+++ house.js
@@ -0,0 +1,30 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+/*
+ *start
+   declare floatMonthlyPayment, intLoanTerm, floatTotalCost
+   get floatMonthlyPayment
+   get intLoanTerm
+   calculate floatTotalCost = floatMonthlyPayment * intLoanTerm
+   output floatTotalCost
+  end
+*/
+ var calculate = function()
+ {
+  var floatMonthlyPayment, intYearsPayment, floatTotalHouseCost;  
+    floatMonthlyPayment= parseFloat($("monthly_payment").value);
+    intYearsPayment= parseInt($("years_of_payment").value);
+    floatTotalHouseCost= parseFloat(floatMonthlyPayment * intYearsPayment);
+    $("total_house_cost").value= floatTotalHouseCost;
+ }
+ 
+ window.onload = function () 
+{
+    $("years_of_payment").value = "";
+    $("total_cost").value = "";
+    $("calc").onclick = calculate;
+    $("monthly_payment").focus();
+}
